c60c8c33feba1ae2cfbf64723ed2b17e206391f3
yunthc <king99ng@snu.ac.kr>
2026-06-02 18:20:48 +0900

    page-clear

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        persona: resolve(__dirname, 'persona.html'),
+        records: resolve(__dirname, 'records.html'),
+        record_detail: resolve(__dirname, 'record_detail.html')
+      }
+    }
+  }
 })
